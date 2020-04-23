572e721b60c41d3b398ab4c72ef514964793a707
yvnpaz <yppousa@esei.uvigo.es>
2020-04-23 22:28:14 +0200

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
---
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
