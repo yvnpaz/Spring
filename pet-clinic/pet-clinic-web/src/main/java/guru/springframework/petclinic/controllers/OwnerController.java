f3e9bc7186ffed9e937c0baa8720c1d6378f82c5
yvnpaz <yppousa@esei.uvigo.es>
2020-04-24 17:27:53 +0200

    Adding owners page and controller. Closes #13

---
+++ pet-clinic/pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package guru.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwner(){
+
+        return "owners/index";
+    }
+}
