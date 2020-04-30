8cc149db2917c91718fd3e4cec4eac98f4eb1e99
yvnpaz <yppousa@esei.uvigo.es>
2020-04-30 21:11:13 +0200

    Add @Entity (manyToMany, oneToMany, ...) and Crud Repository.

---
+++ app-recipe/src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,41 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String categoryName;
+
+//    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "categories")
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
