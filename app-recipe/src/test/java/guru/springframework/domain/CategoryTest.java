048a8d013bc02427a67b17a2c95e057a59670cd2
yvnpaz <yppousa@esei.uvigo.es>
2020-05-03 22:48:53 +0200

    Add Failsafe plugin maven for integration test

--- app-recipe/src/test/java/guru/springframework/domain/CategoryTest.java
+++ app-recipe/src/test/java/guru/springframework/domain/CategoryTest.java
@@ -3,6 +3,9 @@ package guru.springframework.domain;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -21,16 +24,29 @@ public class CategoryTest {
         Long idValue = 4L;
         category.setId(idValue);
         //when
-
         //then
         assertEquals(idValue, category.getId());
     }
 
     @Test
     public void getDescription() {
+        //given
+        String description = "description";
+        category.setDescription(description);
+        //when
+        //then
+        assertEquals(description, category.getDescription());
     }
 
     @Test
     public void getRecipes() {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipeOne = new Recipe();
+        recipes.add(recipeOne);
+        category.setRecipes(recipes);
+        //when
+        //then
+        assertEquals(1, category.getRecipes().size());
     }
 }
