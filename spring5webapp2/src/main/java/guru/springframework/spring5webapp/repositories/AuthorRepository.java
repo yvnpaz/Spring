cea3f8dc480208e7c7da23964c471193507afd5f
yvnpaz <yppousa@esei.uvigo.es>
2020-05-10 12:45:44 +0200

    Delete SpringWebApp from master

--- spring5webapp2/src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.spring5webapp.repositories;
-
-import guru.springframework.spring5webapp.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-
-}
