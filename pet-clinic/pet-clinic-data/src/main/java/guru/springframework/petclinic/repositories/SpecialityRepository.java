f77cb203c18114bf37ad1eba7679c149ad55269c
yvnpaz <yppousa@esei.uvigo.es>
2020-05-10 14:09:59 +0200

    Delete pet-clinic from app-recipe

--- pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/repositories/SpecialityRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.petclinic.repositories;
-
-import guru.springframework.petclinic.model.Speciality;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
-}
--- pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/repositories/VetRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.petclinic.repositories;
-
-import guru.springframework.petclinic.model.Vet;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VetRepository extends CrudRepository<Vet, Long> {
-}
