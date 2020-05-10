d7b09354c4b0871efe29c0ca3f03d70fae9e1287
yvnpaz <yppousa@esei.uvigo.es>
2020-05-10 12:51:52 +0200

    Delete recipe from yp-spring-web-app

--- app-recipe/src/main/java/guru/springframework/repositories/RecipeRepository.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.repositories;
-
-import guru.springframework.domain.Recipe;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-
-}
