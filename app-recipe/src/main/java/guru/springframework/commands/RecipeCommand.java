16642c61b254d9f9618126e8cfb3f22dc366c96b
yvnpaz <yppousa@esei.uvigo.es>
2020-07-08 20:49:39 +0200

    Implement Convert test

---
+++ app-recipe/src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -0,0 +1,27 @@
+package guru.springframework.commands;
+
+import guru.springframework.domain.Difficulty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecipeCommand {
+    public Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    private NotesCommand note;
+    private Set<IngredientCommand> ingredients = new HashSet<>();
+    private Difficulty difficulty;
+    private Set<CategoryCommand> categories = new HashSet<>();
+}
---
+++ app-recipe/src/main/java/guru/springframework/converters/GenericsCommand.java
@@ -0,0 +1,9 @@
+package guru.springframework.converters;
+
+public class GenericsCommand<T, V> {
+
+    public T convertGeneric(T object1, V object2){
+        T obj = null;
+        return obj;
+    }
+}
